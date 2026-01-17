12b0e37e31c59febfa95e96c2172b8fc15792957
Scholtz <ludko2@gmail.com>
2026-01-17 08:29:58 +0100

    Enable TinyPool test and update expected pool JSON
    Removed [Ignore] from LoadTinyPoolAsync to make the test active.
    Updated the expected JSON in assertions to match new pool state
    values, including asset amounts, fees, and timestamp.

--- AVMTradeReporterTests/Processors/Pool/TinyPoolTests.cs
+++ AVMTradeReporterTests/Processors/Pool/TinyPoolTests.cs
@@ -15,7 +15,6 @@ namespace AVMTradeReporterTests.Processors.Pool
     public class TinyPoolTests
     {
         [Test]
-        [Ignore("Requires live Algod mainnet and stable pool state; not suitable for offline/CI runs")]
         public async Task LoadTinyPoolAsync()
         {
             // Arrange
@@ -37,7 +36,7 @@ namespace AVMTradeReporterTests.Processors.Pool
             Assert.That(pool.AssetIdA, Is.EqualTo(3032713424));
             Assert.That(pool.AssetIdB, Is.EqualTo(0));
             var json = Algorand.Utils.Encoder.EncodeToJson(pool);
-            Assert.That(json, Is.EqualTo("{\r\n  \"PoolAddress\": \"2JPTIQWQEWFA6LIHCIXQ24LBDY7ZJ2DHKC3X5FPEITLMDB4C2BDIOXN6QE\",\r\n  \"PoolAppId\": 1002541853,\r\n  \"AssetIdA\": 3032713424,\r\n  \"AssetIdB\": 0,\r\n  \"AssetIdLP\": 3110834238,\r\n  \"A\": 436901768636613115,\r\n  \"B\": 23523128325,\r\n  \"AF\": 291094199274315,\r\n  \"BF\": 11530045,\r\n  \"Protocol\": 1,\r\n  \"Timestamp\": \"2025-08-12T19:41:06.7723426+02:00\",\r\n  \"AMMType\": 0,\r\n  \"ApprovalProgramHash\": \"dd63834ddcd51013ec0a22142497ad4c6d74e421e6c79149422c243346691f56\",\r\n  \"LPFee\": 0.003,\r\n  \"ProtocolFeePortion\": 0.2,\r\n  \"VirtualAmountA\": 437192862835887430.0,\r\n  \"RealAmountA\": 437192862835887430.0,\r\n  \"VirtualAmountB\": 23534658370.0,\r\n  \"RealAmountB\": 23534658370.0\r\n}"));
+            Assert.That(json, Is.EqualTo("{\r\n  \"PoolAddress\": \"2JPTIQWQEWFA6LIHCIXQ24LBDY7ZJ2DHKC3X5FPEITLMDB4C2BDIOXN6QE\",\r\n  \"PoolAppId\": 1002541853,\r\n  \"AssetIdA\": 3032713424,\r\n  \"AssetIdB\": 0,\r\n  \"AssetIdLP\": 3110834238,\r\n  \"A\": 301148413159373969,\r\n  \"B\": 35433109947,\r\n  \"AF\": 82773045982844,\r\n  \"BF\": 10658922,\r\n  \"Protocol\": 1,\r\n  \"Timestamp\": \"2026-01-17T08:28:22.5771015+01:00\",\r\n  \"AMMType\": 0,\r\n  \"ApprovalProgramHash\": \"dd63834ddcd51013ec0a22142497ad4c6d74e421e6c79149422c243346691f56\",\r\n  \"LPFee\": 0.0036,\r\n  \"ProtocolFeePortion\": 0.1111111111111111111111111111,\r\n  \"VirtualAmountA\": 301231186205356813.0,\r\n  \"RealAmountA\": 301231186205356813.0,\r\n  \"VirtualAmountB\": 35443768869.0,\r\n  \"RealAmountB\": 35443768869.0\r\n}"));
 
         }
         [Test]
